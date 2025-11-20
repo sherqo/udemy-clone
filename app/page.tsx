d172ab5f729e2be9628026240a3a920abe2e4742
Sherqo <sharqawycs@gmail.com>
2025-11-20 02:35:44 +0200

    remove the old test banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
 import CourseGrid from '../components/CourseGrid';
 import TrustedCompanies from '../components/TrustedCompanies';
 import Banner from '../components/Banner';
-import FeatureImageRight from '../components/FeatureImageRight';
 import { courses } from '../data/courses';
 
 export default function Home() {
   return (
     <div className="bg-zinc-50 font-sans">
       <Banner />
-      <FeatureImageRight />
       <main className="mx-auto max-w-7xl py-12 px-6">
         <section className="mb-6">
           <CourseGrid courses={courses} />
